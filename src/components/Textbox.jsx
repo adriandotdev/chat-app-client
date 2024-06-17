23ecf61bf56f903d9083f37d4583ca741cf4dbfa
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-06-17 22:14:10 +0800

    install: react-hook-form

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 
-function Textbox({ type, label, placeholder, htmlFor }) {
+function Textbox({
+	type,
+	label,
+	placeholder,
+	htmlFor,
+	register,
+	validation,
+	errors,
+}) {
+	console.log(errors);
 	return (
 		<section className="flex flex-col gap-1 max-w-[30rem] mb-3">
 			<label className="font-semibold" htmlFor={htmlFor}>
 				{label}
 			</label>
 			<input
+				{...register(htmlFor, validation)}
 				type={type}
 				placeholder={placeholder}
-				className="input input-primary w-full"
+				className={`input w-full ${
+					errors[htmlFor] ? "input-error" : "input-primary"
+				}`}
 			/>
+			{errors[htmlFor] && (
+				<small className="text-red-500 font-semibold">
+					{errors[htmlFor].message}
+				</small>
+			)}
 		</section>
 	);
 }
--- src/routes/LoginRoute.jsx
+++ src/routes/LoginRoute.jsx
@@ -1,27 +1,49 @@
 import { Link } from "react-router-dom";
-
+import { useForm } from "react-hook-form";
 // Components
 import Textbox from "../components/Textbox";
 
 function LoginRoute() {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+
+	const SignIn = (data) => {
+		console.log(data);
+	};
 	return (
 		<main className="min-h-[100vh]  flex items-center justify-center">
 			<div className="w-full flex flex-col justify-center max-w-[30rem] p-4">
 				<h1 className="text-xl sm:text-2xl lg:text-4xl mb-5 font-bold text-yellow-400 self-center">
 					Welcome to Humble
 				</h1>
-				<form action="" className="w-full">
+				<form onSubmit={handleSubmit(SignIn)} className="w-full">
 					<Textbox
 						type="text"
 						label="Username"
 						htmlFor="username"
 						placeholder="Username"
+						register={register}
+						validation={{
+							required: "Please provide your username",
+							minLength: {
+								value: 12,
+								message: "Username must be at least 12 characters",
+							},
+						}}
+						errors={errors}
 					/>
 					<Textbox
 						type="password"
 						label="Password"
 						htmlFor="password"
 						placeholder="Password"
+						register={register}
+						validation={{ required: "Please provide your password" }}
+						errors={errors}
 					/>
 					<button className="btn btn-primary w-full mt-5 font-bold text-md">
 						Sign In
